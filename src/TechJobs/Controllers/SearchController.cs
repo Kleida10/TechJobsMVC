a5efab6237b5f9f0b3944b250e66023db67e1114
Kleida10 <56648839+Kleida10@users.noreply.github.com>
2020-03-08 19:45:00 -0500

    DONE

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,29 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> jobs;
+            if (searchType == "all")
+            { 
+                if(searchTerm == null)
+                {
+                   jobs = JobData.FindAll();
+                }
+                else
+                {
+                    jobs = JobData.FindByValue(searchTerm);
 
+                }
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            ViewBag.jobs = jobs;
+            ViewBag.columns = ListController.columnChoices;
+            return View("Index");
+        }
     }
 }
